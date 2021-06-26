2773bf1854d1773c92e82d0dd2e0adab09f000bc
grzeimann <grzeimann@gmail.com>
2021-06-26 07:58:19 -0500

    update

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ Created on Thu May 20 15:06:16 2021
 import numpy as np
 
 stellar_template_file = 'miles_pca_ncomp_10.fits'
-vbins = np.linspace(-500, 500, 25)
-zbins_gal = np.linspace(0.005, 0.47, 47*10+1)
-zbins_qso = np.linspace(0.1, 4.0, int((4.0-0.1)/0.01+1))
+vbins = np.arange(-500, 550, 50)
+zbins_gal = np.arange(0.005, 0.471, .001)
+zbins_qso = np.arange(0.1, 4.01, 0.01)
