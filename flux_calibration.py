d9e233b94cd2225fb528c627b70022b452a34295
grzeimann <grzeimann@gmail.com>
2021-07-14 16:20:06 -0500

    first commit in debug mode

---
+++ flux_calibration.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 14 13:33:23 2021
+
+@author: gregz
+"""
+
+import glob
+import numpy as np
+import os.path as op
+
+from astropy.io import fits
+
+path = '/work/03730/gregz/maverick/classifications'
+filenames = sorted(glob.glob(op.join(path, 'classifications*.fits')))
+
+speclist = []
+for filename in filenames:
+    f = fits.open(filename)
+    is_wd = np.where((f[1].data == 1.) * (f[5].data == 'W'))[0]
+    if len(is_wd) > 0:
+        speclist.append(f[0].data[is_wd])
+speclist = np.vstack(speclist)
+fits.PrimaryHDU(speclist).writeto('out.fits', overwrite=True)
