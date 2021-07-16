624b22d6946e25bf92addc525316bcdcb5b24990
grzeimann <grzeimann@gmail.com>
2021-07-16 10:20:57 -0500

    update

---
+++ match_sdss.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 12:58:06 2021
+
+@author: gregz
+"""
+
+import numpy as np
+
+from astropy.table import Table
+from astropy.io import fits
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+
+t = Table.read("/work/07918/mqd5709/stampede2/sdss_spectra/specObj-dr10.fits")
+ra_sdss = t['PLUG_RA']
+dec_sdss = t["PLUG_DEC"]
+
+f = fits.open("/work/03730/gregz/maverick/parallel/HETVIPS_continuum_v1.0.fits")
+info = f[1].data
+ra_virus = info['ra']
+dec_virus = info['dec']
+coords_sdss = SkyCoord(ra=ra_sdss*u.arcsecond,dec=dec_sdss*u.arcsecond)
+coords_virus = SkyCoord(ra=ra_virus*u.arcsecond,dec=dec_virus*u.arcsecond)
+print('Matching the catalogs')
+idx_sdss,d2d,d3d = coords_virus.match_to_catalog_sky(coords_sdss)
+print('Done matching')
+delta_ra, delta_dec, matches = ([], [], [])
+for i in np.arange(len(idx_sdss)):
+    if d2d.arcsec > 3.:
+        continue
+    matches.append(idx_sdss[i])
+    delta_ra.append(3600. * np.cos(np.deg2rad(dec_virus[i])) * (ra_virus[i] - ra_sdss[idx_sdss[i]]))
+    delta_dec.append(3600. * (dec_virus[i] - dec_sdss[idx_sdss[i]]))
+    
+matches = np.hastack(matches)
+np.savetxt('sdss_matches.txt', matches)
